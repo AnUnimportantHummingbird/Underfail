b37ef4e0f3970b51e5ba58afeb2ebbbfe37c798a
A Person <ai6543r@gmail.com>
2024-10-08 18:06:07 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,3 +2,12 @@
 #* Section
 print("[Insert title here]")
 #*Menu
+print("Options:")
+print("Play(1)")
+print("Story Mode(2)")
+print("Load(3)")
+print("Controls(4)")
+MenuChoice = int()
+while MenuChoice <= 3 and MenuChoice > 0:
+    MenuChoice = input(print("What would you like to choose: "))
+    
