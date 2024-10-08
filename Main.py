4c49b3deb039dbea8efdba7f877b804ff3b6b74c
A Person <ai6543r@gmail.com>
2024-10-08 19:08:32 -0400

    Added a lot of stuff

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
-#Key
-#* Section
+
 print('''
 ╭━━━╮╭━╮╱╭╮
 ┃╭━╮┃┃┃╰╮┃┃
@@ -9,8 +8,26 @@ print('''
 ╰╯╱╰╯╰╯╱╰━┻╯╰┻┻┻┻━━╯''')
 #*Menu
 print("Options:")
-print("Play(1)")
-print("Story Mode(2)")
-print("Load(3)")
-
-
+print("Play")
+print("Story Mode")
+print("Load")
+print("Controls")
+#
+MenuChoice = []
+#Prevents any error messages from occurring
+while True:
+    MenuChoice = input("What would you like to select: ")
+    if MenuChoice == "Play" or MenuChoice == "play":
+        print("This area is a possible work in progress.")
+        break
+    elif MenuChoice == "Story Mode" or MenuChoice == "story mode" or MenuChoice == "Story mode":
+        print("This area is a work in progress.")
+        break
+    elif MenuChoice == "Load" or MenuChoice == "load":
+        print("This feature has not been implemented yet.")
+        break
+    elif MenuChoice == "Load" or MenuChoice == "load":
+        print("This area is a work in progress")
+        break
+    else:
+        print("That option is not available. Try again.")
