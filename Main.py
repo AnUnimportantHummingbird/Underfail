1c48ee085df197f78a6fd3a6aa31be4c9a26653f
A Person <ai6543r@gmail.com>
2024-10-08 18:08:39 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,8 +6,4 @@ print("Options:")
 print("Play(1)")
 print("Story Mode(2)")
 print("Load(3)")
-print("Controls(4)")
-MenuChoice = int()
-while MenuChoice <= 3 and MenuChoice > 0:
-    MenuChoice = input(print("What would you like to choose: "))
-    
+
